2cd2652f872ab353dd497d865099e76a32c98785
andres felipe marchant <andres.marchant.s@uc.cl>
2023-05-20 16:52:19 -0400

    casi listo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,34 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task =  Task.find( params[:id] )
+    @task =  Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save # Will raise
+      redirect_to task_path(@task)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @task =  Task.find(task_params[:id])
+    @task.update(task_params)
+    @task.save
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title,:details,:completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # rails routes -c tasks -E
+  #unicos alias son de new, show y delete
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post  '/tasks',  to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks:edit'
 
-  get '/tasks', to: 'tasks#index', as: :index
-  get '/tasks/:id', to: 'tasks#show', as: :show
 end
